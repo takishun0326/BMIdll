c80a1906d496be9e9efc8214b3d47dfb45e9a97f
takishun <takishun.st3260@gmail.com>
2020-05-31 18:33:38 +0900

    BmiをDll化

--- BmiMain/BmiMain.cpp
+++ BmiMain/BmiMain.cpp
@@ -1,21 +1,24 @@
-﻿// BmiMain.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
-
-#include "pch.h"
+﻿#include "../Dllexecute/Bmi/Bmi.h"
 #include <iostream>
+#include <iomanip>
+#include <vector>
+
+using namespace MyUtil;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	std::vector<Bmi *> bmi;
+	bmi.push_back(new Bmi("yuseisui", 1.65, 50.0));
+	bmi.push_back(new Bmi("takishun", 1.69, 60.1));
+	bmi.push_back(new Bmi("yoko", 1.78, 55.0));
+
+	for (std::vector<Bmi*>::iterator it = bmi.begin(); it != bmi.end(); ++it) {
+		std::cout << (*it)->getName() << "のBMIは"
+			<< std::setprecision(4) << (*it)->getBmi() << " で、" <<
+			(*it)->calc() << "です。" << std::endl;
+	}
+	for (std::vector<Bmi *>::iterator it = bmi.begin(); it != bmi.end(); ++it) {
+		delete *it;
+	}
+	return 0;
 }
-
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
-
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
